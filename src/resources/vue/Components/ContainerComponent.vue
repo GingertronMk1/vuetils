dfca0e4581a8bdc0a7c150cc3b9cfcf1326807d9
Jack Ellis <jackellis1504@gmail.com>
2025-08-06 11:17:53 +0100

    Update container

--- src/resources/vue/Components/ContainerComponent.vue
+++ src/resources/vue/Components/ContainerComponent.vue
@@ -1,7 +1,5 @@
 <template>
-  <div class="w-full flex flex-row justify-center">
-    <div class="container">
-      <slot />
-    </div>
+  <div class="w-full xs:px-[15px] sm:max-w-xl md:max-w-3xl lg:max-w-5xl 2xl:max-w-7xl mx-auto">
+    <slot />
   </div>
 </template>
